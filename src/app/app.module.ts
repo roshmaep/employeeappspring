fb06df7c5d3c965274581c19b9b9d0cb4dd03b0e
roshmaep <roshmaep1999@gmail.com>
2022-12-06 22:19:40 +0530

    view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { ViewemployeeComponent } from './viewemployee/viewemployee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddemployeeComponent
+    AddemployeeComponent,
+    ViewemployeeComponent
   ],
   imports: [
     BrowserModule,
